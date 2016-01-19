3c0f5352809728d36dc5533ecf8ad74fa512c014
julio <caicedo.julio@gmail.com>
2016-01-19 09:59:55 -0430

    dd

--- resources/views/generador_oaca.blade.php
+++ resources/views/generador_oaca.blade.php
@@ -8,12 +8,37 @@
         <div class="row">
             <div class="col-lg-1">
                 <h3>Botones</h3>
-            <button class="btn btn-default">Agregar Texto</button>
+                <button class="btn btn-default" data-toggle="modal" data-target="#AgregarTexto">Agregar Texto</button>
                 <button class="btn btn-default">Agregar Elemento</button>
 
             </div>
         </div>
     </div>
 
+    {{--Modal Agregar Texto--}}
+
+    <div class="modal fade" id="AgregarTexto" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                </div>
+
+                <div class="modal-body">
+                    <p>Body del Modal</p>
+                </div>
+
+                <div class="modal-footer">
+                    <button class="btn btn-default" type="button" data-dismiss="modal">Close</button>
+                    <button class="btn btn-primary" type="button">Save changes</button>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
 
     @endsection
