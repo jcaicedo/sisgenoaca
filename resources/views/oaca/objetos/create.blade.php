9cc9c69bf340b339996d1666c27871202063d5ee
Julio Caicedo <juliocaicedo@Bees-MacBook-Pro.local>
2016-02-16 12:19:07 -0430

    iniciando objetivos

--- resources/views/oaca/objetos/create.blade.php
+++ resources/views/oaca/objetos/create.blade.php
@@ -3,6 +3,35 @@
 @section('title','Create Objeto')
 @section('content')
 
+<script type="text/javascript">
+	
+$(document).ready(function(){
+                   $('#myModal').delay(3500).fadeIn("slow");
+               
+});
+</script>
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 
 <div class="container-fluid">
 	<div class="row">
