48eb2e147c861c24bb29e0312a39c9f8ecc92342
julio <caicedo.julio@gmail.com>
2016-02-28 16:44:23 -0430

    agregando subtitulos dinamicos

--- public/assets/js/objetivos.js
+++ public/assets/js/objetivos.js
@@ -13,11 +13,17 @@ $("#contenido li").last().after("<li><a>"+$("#title").val()+"</a></li>");
 });
 
 $(document).on("click","#add-title",function(e){
-$(".title-content").last().after('<div class="form-group title-content"><div class="col-sm-11"><input class="form-control"></div><div class="col-sm-1"><button type="button" class="btn btn-box-tool" id="btn-add-contri"><i class="fa fa-plus"></i></button></div><div class="col-md-7"><br><input class="form-control"><br><input class="form-control"><br><input class="form-control"></div></div>');
+$(".title-content").last().after('<div class="title-content"><div class="col-sm-11"><input class="form-control"></div><div class="col-sm-1"><button type="button" class="btn btn-box-tool" id="btn-add-subtitle"><i class="fa fa-plus"></i></button></div><div class="col-md-7 sub-title-content" style="left: 16%;"><br></div></div>');
 
 e.preventDefault();
 
 
+});
+
+$(document).on("click",".btn-add-subtitle",function(e){
+
+alert ($(this).attr('id'));
+	e.preventDefault();
 });
 
 
--- resources/views/oaca/objetos/create.blade.php
+++ resources/views/oaca/objetos/create.blade.php
@@ -15,30 +15,27 @@
 
 				<form action="#" method="post" class="form-vertical" id="form_temario">
 					
-					<div class="form-group title-content">
-						
+					<div class="form-group  box box-body">
+						<div class="title-content">
 						<div class="col-sm-11">
 							<input class="form-control">
 						</div>
 
 						<div class="col-sm-1">
-							<button type="button" class="btn btn-box-tool" id="btn-add-contri">
+							<button type="button" class="btn btn-box-tool btn-add-subtitle" id="plus1">
                     <i class="fa fa-plus"></i>
                 </button>
 						</div>
-
-						<div class="col-md-7">
+						<div class="col-md-7 sub-title-content" style="left: 16%;">
 						<br>
-						<input class="form-control">
-						<br>
-						<input class="form-control">
-						<br>
-						<input class="form-control">
-					</div>
-
-
-					</div>
+							<input type="text" class="form-control">
+							<input type="text" class="form-control">
+							<input type="text" class="form-control">
+						</div>
+				</div>
 				
+					</div>
+
 				</form>
 
 						{{-- /.form --}}
