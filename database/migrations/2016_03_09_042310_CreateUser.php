106f025feda57f63d5fcc32452ea5b6cff002cbd
vagrant <vagrant@homestead>
2016-03-10 03:34:46 +0000

    createuser

---
+++ database/migrations/2016_03_09_042310_CreateUser.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user',function (Blueprint $table){
+
+            $table->increments('id');
+            $table->string('first_name')->required();
+            $table->string('last_name')->required();
+            $table->string('email')->unique()->required();
+            $table->string('password')->required();
+            $table->enum('rol',['admin','estudiante','profesor'])->default('profesor');
+            $table->string('organization')->required();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
