759ed87b623fc6717a5aa19508aa3682cc37b4e2
vagrant <vagrant@homestead>
2016-03-10 03:34:26 +0000

    createuser

--- app/Http/Controllers/GeneradorController.php
+++ app/Http/Controllers/GeneradorController.php
@@ -1,65 +1,63 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\RegistroOaca;
 use App\Content;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class GeneradorController extends Controller
 {
 
-
-    function index(){
-
-    }
-
-    function form_register(){
-
-        return view('oaca.registro.create');
-    }
-
-    public function registro(){
-
-        $data = $request->input('data'); //se debe guardar $data en la BD y no el json.
-
-        $json = json_decode($data); // aqui se convierte en json
-        dd($request->input('data'));
-//      $data = Input::all();
-  //    print_r($data);die;
-    //}
-
-
-
-    }
-
-
-    public function view_objetos_create(){
-        return view('oaca.objetos.create');
-    }
-
-    public function create_objetos(Request $request){
-
-        $content= new Content;
-  $confirmacion = array(
-            'status' => 'success',
-            'msg' => 'Setting created successfully',
-        );
-if($request->ajax()){
-    $data=$request->input('obj');
-
-    $response = json_encode($data);
-    $content->contenido=$response;
-    $content->save();
-    return response()->json($confirmacion);
-}else{
-    return 'no';
+function index(){
 }
-    
-    }
 
+function form_register(){
+return view('oaca.registro.create');
+}
 
+public function registro(){
+
+$data = $request->input('data'); //se debe guardar $data en la BD y no el json.
+$json = json_decode($data); // aqui se convierte en json
+
+dd($request->input('data'));
+//      $data = Input::all();
+//    print_r($data);die;
+//}
+}
+
+public function view_objetos_create(){
+
+return view('oaca.objetos.create');
+}
+
+public function create_objetos(Request $request){
+
+$content= new Content;
+
+$confirmacion = array(
+'status' => 'success',
+'msg' => 'Setting created successfully',
+);
+
+if($request->ajax()){
+
+$data=$request->input('obj');
+
+$response = json_encode($data);
+
+$content->contenido=$response;
+
+$content->save();
+
+return response()->json($confirmacion);
+
+}else{
+
+return 'no';
+
+}
+
+}
 }
