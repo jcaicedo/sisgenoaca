9292e5a499355cc5361d21f13549009e5112a30b
caicedo.julio <caicedo.julio@gmail.com>
2016-06-14 23:04:14 -0400

    ajustes preview

--- public/assets/js/objetos/preview.js
+++ public/assets/js/objetos/preview.js
@@ -2,7 +2,11 @@
       e.preventDefault();
 
       postData = $('#form-create-oaca').serializeObject();
-       console.log(postData);
+       console.log(postData['elementos']);
+
+       $elementos = postData['elementos'].split(',');
+
+       console.log($elementos[0]);
 
        $("#form-create-oaca").hide();
        $(".content-preview").show();
