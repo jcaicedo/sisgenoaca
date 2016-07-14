cf6e035311b38056e29f4f2d97e21bfe399987cf
Julio Caicedo <julio.caicedo@bybeeconcep.com>
2016-07-14 15:34:52 -0400

    ajustes

---
+++ app/Models/Users.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Users extends Authenticatable
+{
+    protected $table = 'users';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    'name',
+    'username',
+    'email',
+    'password',
+    'avatar_image'
+    ];
+
+
+
+    protected $hidden = [
+    'password', 'remember_token',
+    ];
+
+
+}
