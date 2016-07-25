61064760cca2f368cf3adb866d56ecd8e08a68dc
Julio Caicedo <julio.caicedo@bybeeconcep.com>
2016-07-25 12:14:30 -0400

    ajuste de table index register

--- resources/views/admin/oaca/registry/index.blade.php
+++ resources/views/admin/oaca/registry/index.blade.php
@@ -23,11 +23,11 @@
 
 		<div class="row">
 			<div class="col-xs-12">
-				<div class="box">
+				<div class="box box-primary">
 					{{-- <div class="box-header">
 						<h3 class="box-title">Mis Oacas</h3>
 					</div> --}}
-					<div class="box-body table-responsive no-padding">
+					<div class="box-body  table-responsive no-padding">
 						
 						<table class="table table-hover">
 							<tr>
