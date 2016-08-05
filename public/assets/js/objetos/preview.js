6ce539544ebe472db5cd6bd13f76fd77d0739270
caicedo.julio <caicedo.julio@gmail.com>
2016-08-05 00:31:05 -0400

    ajustes de nueva estructura de name en los input

--- public/assets/js/objetos/preview.js
+++ public/assets/js/objetos/preview.js
@@ -1,72 +1,61 @@
  $("#preview-oaca").click(function(e){
 
- var postDataObject = $('#form-create-oaca').serializeObject();
-  console.log(postDataObject);
+
+   e.preventDefault();
+
+
+   var countImage = 0;
+
+   $("#form-create-oaca .myinput").each(function(index){
+
+     console.log($(this).data('element'));
+     var element = $(this).data('element');
+     switch(element){
+
+      case 'title':
+      $(".content-preview").append('<h2>'+$(this).val()+'</h2>');
+      break;
+
+      case 'textarea':
+      var content_textarea = $('#'+$(this).attr('id')).summernote('code');
+      $(".content-preview").append('<br>'+content_textarea+'<br>');
+      console.log($(this).data('content'));
+      console.log(content_textarea);
+      var idContent = $(this).data('content');
+      console.log(idContent);
+      $("#"+idContent).val(content_textarea);
+      break;
+
+      case 'image':
+
+      $(".content-preview").append('<br><img src="" alt="'+$(this).attr('id')+'" id="loadimage'+countImage+'" height="100" width="100"><br>');
+
+      $("#"+$(this).attr('id')).html(function(){
+       readImage(this,countImage);
+       countImage ++;
+     });
+      break;
+
+    }
+
+  });
 
 
 
 
+   var myElements = [];
 
-  e.preventDefault();
+   $("#form-create-oaca .myinput").each(function(index){
+     myElements.push($(this).data("element"));
 
-  var myElements = [];
+   });
 
-  $("#form-create-oaca .myinput").each(function(index){
-   myElements.push($(this).data("element"));
+   $(".sortable").hide();
+   $(".box-header-btn").hide();
+   $(".preview").show();
+
 
  });
-
-
-  $("#hidden_elementos").val(myElements);
-
-
-
-  var postData = $('#form-create-oaca').serializeArray();
-  //console.log(postData);
-
-  // var Data = $('#form-create-oaca').serialize();
- 
-  var elementos = postData[1].value.split(',');
- // console.log(postData);
-
-
- numImage = 0;
-
- $.each(postData, function(index, input){
-
-  var   element = input.name;
-
-  switch(element){
-    case 'title':
-    $(".content-preview").append('<h2>'+input.value+'</h2>');
-    break;
-
-    case 'textarea':
-    console.log(input.value);
-    var content_textarea = $('#'+input.value).summernote('code');
-    $(".content-preview").append('<br>'+content_textarea+'<br>');
-
-    break;
-
-    case 'image':
-    $(".content-preview").append('<br><img src="" alt="'+input.value+'" id="loadimage'+numImage+'" height="100" width="100"><br>');
-
-    $("#"+input.value).html(function(){
-     readImage(this,numImage);
-     numImage ++;
-   });
-    break;
-  }
-  //console.log(input.value);
-
-});
-
- $(".sortable").hide();
- $(".box-header-btn").hide();
- $(".preview").show();
-
-
-});
 
 
  $('#preview').click(function(e){
--- resources/views/admin/oaca/objetos/introduction/add.blade.php
+++ resources/views/admin/oaca/objetos/introduction/add.blade.php
@@ -227,9 +227,8 @@
             	$(title).removeClass("nomostrar").addClass("remove-div-"+count).addClass("title").appendTo(this);
             	$(".remove-div-"+count).find('input').attr({"data-element":"title","data-position":count,"id":"title-"+count,"name":"data["+count+"][content]"}).addClass("myinput");
             	$(".remove-div-"+count).find('button').attr({"data-parent":"remove-div-"+count}).addClass('remove-div');
-            	//$(".remove-div-"+count).after("<input type='hidden' name='data["+count+"][content]' value='title-"+count+"'>");
-            	$(".remove-div-"+count).after("<input type='hidden' name='data["+count+"][type]' value='title'>");
-            	$(".remove-div-"+count).after("<input type='hidden' name='data["+count+"][id]' value=''>");
+            	$("#title-"+count).after("<input type='hidden' name='data["+count+"][type]' value='title'>");
+            	$("#title-"+count).after("<input type='hidden' name='data["+count+"][id]' value=''>");
 
 
             	count ++;
@@ -239,9 +238,12 @@
             	case 'textarea':
             	var textarea = $(".textareaclone").clone();
             	$(textarea).removeClass("nomostrar").removeClass("textareaclone").addClass("remove-div-"+count).addClass("textarea").appendTo( this );
-            	$(".remove-div-"+count).find('.edit-textarea').attr({"data-element":"textarea","data-position":count,'id':'textarea'+count,"name":"textarea"}).addClass("myinput").after("<input type='hidden' name='textarea' id='input-textarea"+count+"' value='textarea"+count+"'>");
+            	$(".remove-div-"+count).find('.edit-textarea').attr({"data-element":"textarea","data-position":count,'data-content':'content-textarea'+count,'id':'textarea'+count,"name":"textarea"}).addClass("myinput");
             	$(".remove-div-"+count).find("input#input-textarea"+count).addClass('componente');
             	$(".remove-div-"+count).find('button').attr({"data-parent":"remove-div-"+count}).addClass('remove-div');
+            	$("#textarea"+count).after("<input type='hidden' name='data["+count+"][content]' id='content-textarea"+count+"' value='pruab'>");
+            	$("#textarea"+count).after("<input type='hidden' name='data["+count+"][type]'  value='textarea'>");
+            	$("#textarea"+count).after("<input type='hidden' name='data["+count+"][id]' value='00'>");
 
 
             	$('#textarea'+count).summernote({
@@ -261,10 +263,13 @@
             	var uploadimage = $("div.uploadimage-clone").clone();
             	$(uploadimage).removeClass("nomostrar").removeClass("uploadimage-clone").addClass("remove-div-"+count).appendTo(this)
 
-            	$(".remove-div-"+count).find('input').attr({"data-element":"image","data-position":count,'value':'image-'+count,"name":"image","id":'imagep-'+count}).addClass("myinput").after("<input type='hidden' id='image-"+count+"' name='image' value='imagep-"+count+"'>");
-            	
+            	$(".remove-div-"+count).find('input').attr({"data-element":"image","data-position":count,'value':'image-'+count,"name":"image","id":'imagep-'+count}).addClass("myinput");
             	$(".remove-div-"+count).find('button').attr({"data-parent":"remove-div-"+count}).addClass('remove-div');
             	$("#image-"+count).addClass("componente");
+            	$(".remove-div-"+count).after("<input type='hidden' id='image-"+count+"' name='image' value='imagep-"+count+"'>");
+            	$(".remove-div-"+count).after("<input type='hidden' name='data["+count+"][content]' value=''>");
+            	$(".remove-div-"+count).after("<input type='hidden' name='data["+count+"][type]' value='image'>");
+            	$(".remove-div-"+count).after("<input type='hidden' name='data["+count+"][id]' value=''>");
 
             	count ++;
 
