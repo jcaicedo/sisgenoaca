5f924abfe2342bee741f30cdcf5aae667c11eb79
Julio Caicedo <julio.caicedo@bybeeconcep.com>
2016-08-29 09:10:23 -0400

    implementación view plantilla

--- resources/views/oaca/pages/oaca.blade.php
+++ resources/views/oaca/pages/oaca.blade.php
@@ -5,12 +5,23 @@
 <div class="content-wrapper">
 
 	<section class="introduction">
+		
 		@foreach($content_introduction as $key=>$element_introduction)
 
+		@if($element_introduction->type_element == 'title')
+
+		<h1>{{ $element_introduction->content }}</h1>
+
+		@endif
+
 		@if($element_introduction->type_element == 'textarea')
+
+		<?php echo $element_introduction->content ?>
 		
-		<input type="hidden" class="value-textarea" name="textarea[]" value = "{{$element_introduction->content}}">
-		
+		@endif
+
+		@if($element_introduction->type_element == 'image')
+		<img src="{{ $element_introduction->content }}" alt="" style="width:50px; height:100%">
 		@endif
 
 		@endforeach
@@ -34,11 +45,6 @@
 
 	$(document).ready(function (e) {
 		
-		$('.value-textarea').each(function(index){
-
-			$(this).after($(this).val());
-
-		});
 
 
 
