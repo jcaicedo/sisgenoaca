7a453595c3c07457e55de95b8e41898e7c989fcd
caicedo.julio <caicedo.julio@gmail.com>
2016-10-14 00:29:48 -0400

    ajustes

--- app/Http/Controllers/Admin/GeneradorController.php
+++ app/Http/Controllers/Admin/GeneradorController.php
@@ -60,7 +60,7 @@ class GeneradorController extends AdminController
 	}
 
 	function postIntroduction(Request $request){
-		dd($request->input('data'));
+		//dd($request->input('data'));
 		
 		$Arr = explode(",",$request->input('elementos-delete'));
 		$collection = ElementsOaca::destroy($Arr);
