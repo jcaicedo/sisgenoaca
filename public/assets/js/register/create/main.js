8911240b04edf2e2e76919453cdb706405482c99
caicedo.julio <caicedo.julio@gmail.com>
2016-10-20 22:38:41 -0400

    incidencia

--- public/assets/js/register/create/main.js
+++ public/assets/js/register/create/main.js
@@ -1,46 +1,46 @@
 jQuery(document).ready(function($) {
 	var count = 1;
-		var count_words_key=2;
+	var count_words_key=2;
 
 
-		/*add Words Key*/
+	/*add Words Key*/
 
-		$('.btn-wordskey-plus').click(function(e){
-			e.preventDefault();
+	$('.btn-wordskey-plus').click(function(e){
+		e.preventDefault();
 
-			var input_wordKey = $('.box-wordkey-clone').clone().addClass('box-wordkey').removeClass('box-wordkey-clone').removeClass('nomostrar').attr({'id':'divwordkey'+count_words_key});
-			input_wordKey.find('button.btn-remove').data('parent','divwordkey'+count_words_key);
-			input_wordKey.find('input').attr({'name':'words_key[]','id':'words_key'+count_words_key});
-			count_words_key++;
-			$('.box-wordskeys').append(input_wordKey);
+		var input_wordKey = $('.box-wordkey-clone').clone().addClass('box-wordkey').removeClass('box-wordkey-clone').removeClass('nomostrar').attr({'id':'divwordkey'+count_words_key});
+		input_wordKey.find('button.btn-remove').data('parent','divwordkey'+count_words_key);
+		input_wordKey.find('input').attr({'name':'words_key[]','id':'words_key'+count_words_key});
+		count_words_key++;
+		$('.box-wordskeys').append(input_wordKey);
 
-		});
-
-
-		/*Add Colaborators*/
-
-		$('.btn-colaborators').click(function(e){
-			e.preventDefault();
-
-			var colaboratorbox = $('.box-colaborator-clone').clone().removeClass('box-colaborator-clone').removeClass('nomostrar').addClass('box-colaborator').attr('id','divcolaborator'+count);
-			colaboratorbox.find('.box-title').text('Colaborador '+count);
-			colaboratorbox.find('button.btn-remove').data('parent','divcolaborator'+count);
-			colaboratorbox.find("input[id='name']").attr({'id':'name'+count,'name':'colaborator['+count+'][name]'});
-			colaboratorbox.find("input[id='lastname']").attr({'id':'lastname'+count,'name':'colaborator['+count+'][lastname]'});
-			colaboratorbox.find("input[id='email']").attr({'id':'email'+count,'name':'colaborator['+count+'][email]'});
-			colaboratorbox.find("input[id='organization']").attr({'id':'organization'+count,'name':'colaborator['+count+'][organization]'});
-			colaboratorbox.find("select[id='typecontribution']").attr({'id':'typecontribution'+count,'name':'colaborator['+count+'][typecontribution]'});
+	});
 
 
-			count ++;
+	/*Add Colaborators*/
 
-			$('.content-box-colaborators').append(colaboratorbox);
-			console.log(colaboratorbox.html());
+	$('.btn-colaborators').click(function(e){
+		e.preventDefault();
 
-		});
+		var colaboratorbox = $('.box-colaborator-clone').clone().removeClass('box-colaborator-clone').removeClass('nomostrar').addClass('box-colaborator').attr('id','divcolaborator'+count);
+		colaboratorbox.find('.box-title').text('Colaborador '+count);
+		colaboratorbox.find('button.btn-remove').data('parent','divcolaborator'+count);
+		colaboratorbox.find("input[id='name']").attr({'id':'name'+count,'name':'colaborator['+count+'][name]'});
+		colaboratorbox.find("input[id='lastname']").attr({'id':'lastname'+count,'name':'colaborator['+count+'][lastname]'});
+		colaboratorbox.find("input[id='email']").attr({'id':'email'+count,'name':'colaborator['+count+'][email]'});
+		colaboratorbox.find("input[id='organization']").attr({'id':'organization'+count,'name':'colaborator['+count+'][organization]'});
+		colaboratorbox.find("select[id='typecontribution']").attr({'id':'typecontribution'+count,'name':'colaborator['+count+'][typecontribution]'});
 
 
-		$('#form_register').on('click','button.btn-remove',function(e) {
+		count ++;
+
+		$('.content-box-colaborators').append(colaboratorbox);
+		console.log(colaboratorbox.html());
+
+	});
+
+
+	$('#form_register').on('click','button.btn-remove',function(e) {
 					// body...
 					e.preventDefault();
 
@@ -49,99 +49,98 @@ jQuery(document).ready(function($) {
 
 
 		 //iCheck for checkbox and radio inputs
-		  $('input[type="radio"].minimal').iCheck({
-		  	checkboxClass: 'icheckbox_minimal-blue',
-		  	radioClass: 'iradio_minimal-blue'
-		  });
+		 $('input[type="radio"].minimal').iCheck({
+		 	checkboxClass: 'icheckbox_minimal-blue',
+		 	radioClass: 'iradio_minimal-blue'
+		 });
 
 
-		  $('ol').selectable({
-		  	selected: function( event, ui ) {
-		  		$('.input_plantilla').each(function(){
-		  			$(this).remove();
-		  		});
-		  		var idPlantilla = $( ".ui-selected", this ).find('img').attr('name');
-		  		$( ".ui-selected", this ).find('img').after("<input type='hidden' class='input_plantilla' name='plantilla' value='"+idPlantilla+"'>");
+		 $('ol').selectable({
+		 	selected: function( event, ui ) {
+		 		$('.input_plantilla').each(function(){
+		 			$(this).remove();
+		 		});
+		 		var idPlantilla = $( ".ui-selected", this ).find('img').attr('name');
+		 		$( ".ui-selected", this ).find('img').after("<input type='hidden' class='input_plantilla' name='plantilla' value='"+idPlantilla+"'>");
 
-		  	},
-		  	stop: function(event, ui){
-		  		$(".ui-selected:first",this).each(function(){
-		  			$(this).siblings().removeClass("ui-selected");
+		 	},
+		 	stop: function(event, ui){
+		 		$(".ui-selected:first",this).each(function(){
+		 			$(this).siblings().removeClass("ui-selected");
 
-		  		});
-		  	}
-		  });
-
-	});
-
-
-	
-
-	/*Buttom Next*/
-		$('.btn-next').click(function(e){
-			e.preventDefault();
-
-
-			var content_body = $('.btn-next').data('body');
-			console.log(content_body);
-			switch(content_body){
-
-				case 'select-pattern':
-				$('.btn-next').data('body','general-features');
-				$('#'+content_body).hide();
-				console.log('#'+content_body);
-				$('#general-features').show();
-				$('.btn-back').data('body','general-features').show();
-				break;
-
-				case 'general-features':
-				$('.btn-next').data('body','lifecycle');
-				$('#'+content_body).hide();
-				$('#lifecycle').show();
-				$('.btn-back').data('body','lifecycle').show();
-				break;
-
-				case 'lifecycle':
-				$('.btn-next').data('body','educational');
-				$('#'+content_body).hide();
-				$('#educational').show();
-				$('.btn-back').data('body','educational');
-				break;
-
-				case 'educational':
-				$('.btn-next').data('body','copyright');
-				$('#'+content_body).hide();
-				$('#copyright').show();
-				$('.btn-back').data('body','copyright');
-				break;
-
-				case 'copyright':
-				$('.btn-next').data('body','selectable_plantilla');
-				$('#'+content_body).hide();
-				$('#selectable_plantilla').show();
-				$(this).hide();
-				$('.btn-back').data('body','selectable_plantilla');
-				$('.btn-save').show();
-
-			}
+		 		});
+		 	}
+		 });
 
 		});
 
-		/*Button Back*/
 
-		$(".btn-back").click(function(e) {
+
+
+/*Buttom Next*/
+$('.btn-next').click(function(e){
+	e.preventDefault();
+
+
+	var content_body = $('.btn-next').data('body');
+	console.log(content_body);
+	switch(content_body){
+
+		case 'select-pattern':
+		$('.btn-next').data('body','general-features');
+		$('#'+content_body).hide();
+		$('#general-features').show();
+		$('.btn-back').data('body','general-features').show();
+		break;
+
+		case 'general-features':
+		$('.btn-next').data('body','lifecycle');
+		$('#'+content_body).hide();
+		$('#lifecycle').show();
+		$('.btn-back').data('body','lifecycle').show();
+		break;
+
+		case 'lifecycle':
+		$('.btn-next').data('body','educational');
+		$('#'+content_body).hide();
+		$('#educational').show();
+		$('.btn-back').data('body','educational');
+		break;
+
+		case 'educational':
+		$('.btn-next').data('body','copyright');
+		$('#'+content_body).hide();
+		$('#copyright').show();
+		$('.btn-back').data('body','copyright');
+		break;
+
+		case 'copyright':
+		$('.btn-next').data('body','selectable_plantilla');
+		$('#'+content_body).hide();
+		$('#selectable_plantilla').show();
+		$(this).hide();
+		$('.btn-back').data('body','selectable_plantilla');
+		$('.btn-save').show();
+
+	}
+
+});
+
+/*Button Back*/
+
+$(".btn-back").click(function(e) {
 			// body...
 			e.preventDefault();
 
 			var content_body = $('.btn-back').data('body');
-
+			console.log(content_body);
 			switch(content_body){
 
 				case 'general-features':
 				$(this).hide();
 				$('#'+content_body).hide();
-				$('#select_pattern').show();
-				$('.btn-next').data('body','general-features');
+				$('#select-pattern').show();
+				$('.btn-next').data('body','select-pattern');
 				break;
 
 				case 'lifecycle':
--- resources/views/admin/oaca/registry/create/pattern.blade.php
+++ resources/views/admin/oaca/registry/create/pattern.blade.php
@@ -1,37 +1,37 @@
-<div class="box-body" id="select_pattern">
-							<h4>{{trans('admin.select_pattern')}}</h4>
-							<br>
-							<p>Selección un patrón tecnopedagógico a utilizar. Cada una tiene su explicación
-								para su mejor elección.</p>
+<div class="box-body" id="select-pattern">
+	<h4>{{trans('admin.select_pattern')}}</h4>
+	<br>
+	<p>Selección un patrón tecnopedagógico a utilizar. Cada una tiene su explicación
+		para su mejor elección.</p>
 
-								<div class="form-group">
-									
-									<div class="col-md-1 col-md-offset-2">
-										<input class="minimal" name="patter" value="1" type="radio">
-									</div>
-									<div class="col-md-6">
-										<div class="box box-default collapsed-box">
-											<div class="box-header with-border">
-												<h3 class="box-title">DARC</h3>
+		<div class="form-group">
 
-												<div class="box-tools pull-right">
-													<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
-													</button>
-												</div>
-												<!-- /.box-tools -->
-											</div>
-											<!-- /.box-header -->
-											<div class="box-body">
-												Explicación de patrón DARC.
-											</div>
-											<!-- /.box-body -->
-										</div>
-										<!-- /.box -->
-									</div>
-									
+			<div class="col-md-1 col-md-offset-2">
+				<input class="minimal" name="patter" value="1" type="radio">
+			</div>
+			<div class="col-md-6">
+				<div class="box box-default collapsed-box">
+					<div class="box-header with-border">
+						<h3 class="box-title">DARC</h3>
+
+						<div class="box-tools pull-right">
+							<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+							</button>
+						</div>
+						<!-- /.box-tools -->
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
+						Explicación de patrón DARC.
+					</div>
+					<!-- /.box-body -->
+				</div>
+				<!-- /.box -->
+			</div>
 
 
-									
-								</div>
 
-							</div>
+
+		</div>
+
+	</div>
