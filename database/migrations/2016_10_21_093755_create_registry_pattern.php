4d9620d2ebf927e71a5610b3583cc1f6dba512c0
caicedo.julio <caicedo.julio@gmail.com>
2016-10-21 05:46:32 -0400

    arreglo de migrations

---
+++ database/migrations/2016_10_21_093755_create_registry_pattern.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistryPattern extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registry_pattern',function(Blueprint $table){
+
+            $table->uuid('id_registry')
+            ->unsigned();
+            $table->integer('id_pattern')
+            ->unsigned();
+
+            $table->foreign('id_registry')
+            ->references('id')
+            ->on('registrooaca')
+            ->onDelete('cascade');
+
+            $table->foreign('id_pattern')
+            ->references('id')
+            ->on('patterns')
+            ->onDelete('cascade')
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExist('registry_pattern');
+    }
+}
