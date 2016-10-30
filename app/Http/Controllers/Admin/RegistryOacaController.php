d4faef02d03f41697f3860ac4988bd4e261a2427
caicedo.julio <caicedo.julio@gmail.com>
2016-10-30 15:39:16 -0400

    comentando

--- app/Http/Controllers/Admin/RegistryOacaController.php
+++ app/Http/Controllers/Admin/RegistryOacaController.php
@@ -81,7 +81,11 @@ class RegistryOacaController extends Controller
 
 	public function postEdit(Request $request){
 
-		$array_colaborators = [];
+		$array_colaborators = []; //array para guardar los id's de los colaboradores que deben quedarse
+
+		/*
+		*Guardando los ajuses de registro editado
+		*/
 		$register_edited = RegistroOaca::find($request->input('register_id'));
 		$content = json_encode($request->input());
 		$register_edited->content_register = $content;
@@ -91,21 +95,40 @@ class RegistryOacaController extends Controller
 		$register_edited->licencia  = $request->input('licencia');
 		$register_edited->save();
 
+		/*
+		*Se recorren los colaboradores para ingresarlos
+		*al modelo correspondiente.
+		*por cada uno agregado o modificado se guarda en el array_colaborators
+		*/
+
 		foreach ($request->input('colaborator') as $key => $colaborator) {
 			$image = $request->file('colaborator')[$key]["image_organization"];
 			$saved = $this->saveColaborators($colaborator,$register_edited->id, $image);
 			$array_colaborators[$key] = $saved->id;
 		}
-		Colaborators::deleteNotColaborators($array_colaborators,$register_edited->id);
-		$id_pattern = $request->input('pattern');
 
+		/*
+		* Se pasa a deleteNotColaborators el arrary para eliminar los colaborators
+		* que ya no pertenecen al registry
+		*/
+		Colaborators::deleteNotColaborators($array_colaborators,$register_edited->id);
+
+		/*
+		*Se actualiza la informacion respecto al patron que esta utilizando el nuevo registro.
+		*/
+		$id_pattern = $request->input('pattern');
 		$relation_pattern = RegistryPattern::firstOrCreate(['id_registry'=>$register_edited->id]);
 		$relation_pattern->id_pattern = $id_pattern;
 		$relation_pattern->save();
+
 		return redirect('/admin/oaca/registry/registrys');
 
 
 	}
+
+	//////////////////////////////////////////////////
+	///////////////Eliminar Registry//////////////////
+	//////////////////////////////////////////////////
 
 	public function delete(Request $request, $id)
 	{
@@ -116,8 +139,12 @@ class RegistryOacaController extends Controller
 		return redirect('admin/oaca/registry/registrys');
 	}
 
+	////////////////////////////////////////////////////////////
+	//////Function para guardar o editar los colaboradores//////
+	////////////////////////////////////////////////////////////
+
 	public function saveColaborators($colaborator, $id_registry, $image){
-		//dd($colaborator);
+		
 		$newcolaborator = Colaborators::firstOrNew(['id'=>$colaborator['id']]);
 		$newcolaborator->id_registry = $id_registry;
 		$newcolaborator->name = $colaborator['name'];
