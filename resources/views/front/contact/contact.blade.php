d38450b76e14e2aadc86216ff617cdee326df3f1
caicedo.julio <caicedo.julio@gmail.com>
2016-11-28 00:18:01 -0400

    ajustes y reestructuracion de archivos

--- resources/views/front/contact/contact.blade.php
+++ resources/views/front/contact/contact.blade.php
@@ -1,159 +1,60 @@
+@extends('front.layouts.main')
+@section('title','Contacto')
+  @section('content')
+    
+    @include('front.contact.includes.banner-principal')
+    <!-- end:header-top -->
 
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Pentagon &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FREEHTML5.CO" />
-
-  <!--
-	//////////////////////////////////////////////////////
-
-	FREE HTML5 TEMPLATE
-	DESIGNED & DEVELOPED by FREEHTML5.CO
-
-	Website: 		http://freehtml5.co/
-	Email: 			info@freehtml5.co
-	Twitter: 		http://twitter.com/fh5co
-	Facebook: 		https://www.facebook.com/fh5co
-
-	//////////////////////////////////////////////////////
-	 -->
-
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
-
-	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
-
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="themes/pentagon/css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="themes/pentagon/css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="themes/pentagon/css/bootstrap.css">
-	<!-- Superfish -->
-	<link rel="stylesheet" href="themes/pentagon/css/superfish.css">
-
-	<link rel="stylesheet" href="themes/pentagon/css/style.css">
-
-
-	<!-- Modernizr JS -->
-	<script src="themes/pentagon/js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-
-	</head>
-	<body>
-		<div id="fh5co-wrapper">
-		<div id="fh5co-page">
-	      @include('front.partials.menu')
-
-
-        <div class="fh5co-hero fh5co-hero-2">
-          <div class="fh5co-overlay"></div>
-          <div class="fh5co-cover fh5co-cover_2 text-center" data-stellar-background-ratio="0.5" style="background-image: url(assets/imgs/home/banner-principal.png);">
-            <div class="desc animate-box">
-              <h2><strong>Contáctanos</strong></h2>
-
+    <div id="fh5co-contact" class="animate-box">
+      <div class="container">
+        <form action="#">
+          <div class="row">
+            <div class="col-md-6">
+              <h3 class="section-title">Dirección</h3>
+              {{-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p> --}}
+              <ul class="contact-info">
+                <li><i class="icon-location-pin"></i>Universidad Central de Venezuela</li>
+                {{-- <li><i class="icon-phone2"></i>+ 1235 2355 98</li> --}}
+                <li><i class="icon-mail"></i><span>Coordinadora: </span><a href="#"> yoslyhernandez@gmail.com</a> </li>
+                <li><i class="icon-mail"></i><a href="#">proyectoaca@gmail.com</a></li>
+                <li><i class="icon-twitter2"></i><a href="#">@proyectoOACA</a></li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Nombre">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Email">
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <textarea name="" class="form-control" id="" cols="30" rows="7" placeholder="Mensaje"></textarea>
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <input type="submit" value="Enviar" class="btn btn-primary">
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-		<!-- end:header-top -->
+        </form>
+      </div>
+    </div>
+    <!-- END fh5co-contact -->
+    <div id="map" class="fh5co-map"></div>
+    <!-- END map -->
+  @endsection
 
-		<div id="fh5co-contact" class="animate-box">
-			<div class="container">
-				<form action="#">
-					<div class="row">
-						<div class="col-md-6">
-							<h3 class="section-title">Our Address</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-							<ul class="contact-info">
-								<li><i class="icon-location-pin"></i>198 West 21th Street, Suite 721 New York NY 10016</li>
-								<li><i class="icon-phone2"></i>+ 1235 2355 98</li>
-								<li><i class="icon-mail"></i><a href="#">info@yoursite.com</a></li>
-								<li><i class="icon-globe2"></i><a href="#">www.yoursite.com</a></li>
-							</ul>
-						</div>
-						<div class="col-md-6">
-							<div class="row">
-								<div class="col-md-6">
-									<div class="form-group">
-										<input type="text" class="form-control" placeholder="Name">
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="form-group">
-										<input type="text" class="form-control" placeholder="Email">
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<textarea name="" class="form-control" id="" cols="30" rows="7" placeholder="Message"></textarea>
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<input type="submit" value="Send Message" class="btn btn-primary">
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-		<!-- END fh5co-contact -->
-		{{-- <div id="map" class="fh5co-map"></div> --}}
-		<!-- END map -->
-	  @include('front.partials.footer')
-
-
-	</div>
-	<!-- END fh5co-page -->
-
-	</div>
-	<!-- END fh5co-wrapper -->
-
-	<!-- jQuery -->
-
-
-	<script src="themes/pentagon/js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="themes/pentagon/js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="themes/pentagon/js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="themes/pentagon/js/jquery.waypoints.min.js"></script>
-	<!-- Stellar -->
-	<script src="themes/pentagon/js/jquery.stellar.min.js"></script>
-	<!-- Superfish -->
-	<script src="themes/pentagon/js/hoverIntent.js"></script>
-	<script src="themes/pentagon/js/superfish.js"></script>
-	<!-- Google Map -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
-	<script src="themes/pentagon/js/google_map.js"></script>
-	<!-- Main JS (Do not remove) -->
-	<script src="themes/pentagon/js/main.js"></script>
-
-	</body>
-</html>
+  @push('scripts')
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+    <script src="themes/pentagon/js/google_map.js"></script>
+  @endpush
---
+++ resources/views/front/layouts/main.blade.php
@@ -0,0 +1,103 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SISGEOACA - @yield('title')</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+  <meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+  <meta name="author" content="FREEHTML5.CO" />
+
+  <!--
+  //////////////////////////////////////////////////////
+
+  FREE HTML5 TEMPLATE
+  DESIGNED & DEVELOPED by FREEHTML5.CO
+
+  Website: 		http://freehtml5.co/
+  Email: 			info@freehtml5.co
+  Twitter: 		http://twitter.com/fh5co
+  Facebook: 		https://www.facebook.com/fh5co
+
+  //////////////////////////////////////////////////////
+-->
+
+<!-- Facebook and Twitter integration -->
+<meta property="og:title" content=""/>
+<meta property="og:image" content=""/>
+<meta property="og:url" content=""/>
+<meta property="og:site_name" content=""/>
+<meta property="og:description" content=""/>
+<meta name="twitter:title" content="" />
+<meta name="twitter:image" content="" />
+<meta name="twitter:url" content="" />
+<meta name="twitter:card" content="" />
+
+<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+<link rel="shortcut icon" href="favicon.ico">
+
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+
+<!-- Animate.css -->
+<link rel="stylesheet" href="themes/pentagon/css/animate.css">
+<!-- Icomoon Icon Fonts-->
+<link rel="stylesheet" href="themes/pentagon/css/icomoon.css">
+<!-- Bootstrap  -->
+<link rel="stylesheet" href="themes/pentagon/css/bootstrap.css">
+<!-- Superfish -->
+<link rel="stylesheet" href="themes/pentagon/css/superfish.css">
+
+<link rel="stylesheet" href="themes/pentagon/css/style.css">
+
+@stack('styles')
+<!-- Modernizr JS -->
+<script src="themes/pentagon/js/modernizr-2.6.2.min.js"></script>
+<!-- FOR IE9 below -->
+<!--[if lt IE 9]>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+  <div id="fh5co-wrapper">
+    <div id="fh5co-page">
+
+      @include('front.partials.menu')
+
+      @yield('content')
+
+      @include('front.partials.footer')
+
+
+    </div>
+    <!-- END fh5co-page -->
+
+  </div>
+  <!-- END fh5co-wrapper -->
+
+  <!-- jQuery -->
+
+
+  <script src="themes/pentagon/js/jquery.min.js"></script>
+  <!-- jQuery Easing -->
+  <script src="themes/pentagon/js/jquery.easing.1.3.js"></script>
+  <!-- Bootstrap -->
+  <script src="themes/pentagon/js/bootstrap.min.js"></script>
+  <!-- Waypoints -->
+  <script src="themes/pentagon/js/jquery.waypoints.min.js"></script>
+  <!-- Stellar -->
+  <script src="themes/pentagon/js/jquery.stellar.min.js"></script>
+  <!-- Superfish -->
+  <script src="themes/pentagon/js/hoverIntent.js"></script>
+  <script src="themes/pentagon/js/superfish.js"></script>
+
+  <!-- Main JS (Do not remove) -->
+  <script src="themes/pentagon/js/main.js"></script>
+  @stack('scripts')
+</body>
+</html>
